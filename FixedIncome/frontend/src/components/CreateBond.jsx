fa08a9f8005bc0df0d1d99ecd84b53548b296f87
Akbaraliev Erlan <erlan.akbaralievich@gmail.com>
2026-03-20 21:56:22 +0100

    Adds Create Issuer, Bond forms without validation

--- FixedIncome/frontend/src/components/CreateBond.jsx
+++ FixedIncome/frontend/src/components/CreateBond.jsx
@@ -5,11 +5,32 @@ import AddBoxIcon from '@mui/icons-material/AddBox';
 import Button from '@mui/material/Button';
 import TextForm from './Forms/TextForm';
 import SelectForm from './Forms/SelectForm';
-import DateTimePicker from './Forms/DateTimePicker';
+import DatePickerForm from './Forms/DatePickerForm';
+import { useFormik } from 'formik'; 
 
 const CreateBond = () => {
     const [meta, setMeta] = useState(null)
     const [issuers, setIssuers] = useState([])
+    const formik = useFormik({
+        initialValues: {
+            name:'',
+            isin:'',
+            issuer:'',
+            bond_type:'',
+            face_value:'',
+            coupon_rate:'',
+            issue_date:'',
+            maturity_date:''
+        },
+
+        onSubmit: (values)=>{
+            AxiosInstance.post('bonds/', values)
+            .then(()=>{
+                console.log("Successful data submission")
+            })
+        }
+    })
+    console.log(formik.values)
 
     useEffect(()=>{
         AxiosInstance.get('api/meta/').then((res)=>{
@@ -19,7 +40,7 @@ const CreateBond = () => {
             setIssuers(res.data.results)
         })
     },[])
-    if (!meta || !issuers) return null;
+    if (!meta || issuers.length === 0) return null;
 
     return (
         <div>
@@ -27,27 +48,70 @@ const CreateBond = () => {
                 <AddBoxIcon/>
                 <Typography sx={{marginLeft:'15px'}}>Create Bond</Typography>
             </Box>
-
-            <Box sx={{
-                display: 'grid',
-                gridTemplateColumns: {xs: '1fr', md: '1fr 1fr', xl: '1fr 1fr 1fr'},
-                gap:'30px',
-                padding: '15px',
-                boxShadow: 'rgba(100,100,111,0.2) 0px 7px 29px 0px',
-            }}>
-                <TextForm label={'ISIN'}/>
-                <SelectForm label={'Issuer'} options={issuers}/>
-                <SelectForm label={'Credit Rating'} options={meta.credit_ratings}/>
-                <SelectForm label={'Bond Type'} options={meta.bond_types}/>
-                <TextForm label={'Face Value'}/>
-                <TextForm label={'Coupon Rate'}/>
-                <DateTimePicker label={'Issue Date'}/>
-                <DateTimePicker label={'Maturity Date'}/>
-                <Box sx={{ gridColumn: { md: '1 / -1' } }}>
-                    <Button variant="contained" fullWidth>Submit</Button>
+            <form onSubmit={formik.handleSubmit}>
+                <Box sx={{
+                    display: 'grid',
+                    gridTemplateColumns: {xs: '1fr', md: '1fr 1fr'},
+                    gap:'30px',
+                    padding: '15px',
+                    boxShadow: 'rgba(100,100,111,0.2) 0px 7px 29px 0px',
+                }}>
+                    <Box sx={{ gridColumn: {md: '1/-1'} }}>
+                        <TextForm 
+                            label={'ISIN'}
+                            name='isin' 
+                            value={formik.values.isin}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                        />
+                    </Box>
+                    <SelectForm 
+                        label={'Issuer'} 
+                        options={issuers}
+                        name='issuer' 
+                        value={formik.values.issuer}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    <SelectForm 
+                        label={'Bond Type'}
+                        options={meta.bond_types}
+                        name='bond_type' 
+                        value={formik.values.bond_type}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    <TextForm 
+                        label={'Face Value'}
+                        name='face_value' 
+                        value={formik.values.face_value}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    <TextForm 
+                        label={'Coupon Rate'}
+                        name='coupon_rate' 
+                        value={formik.values.coupon_rate}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    <DatePickerForm
+                        label={'Issue Date'}
+                        name='issue_date'
+                        value={formik.values.issue_date}
+                        onChange={formik.setFieldValue}
+                    />
+                    <DatePickerForm
+                        label={'Maturity Date'}
+                        name='maturity_date'
+                        value={formik.values.maturity_date}
+                        onChange={formik.setFieldValue}
+                    />
+                    <Box sx={{ gridColumn: { md: '1 / -1' } }}>
+                        <Button type="submit" variant="contained" fullWidth>Submit</Button>
+                    </Box>
                 </Box>
-
-            </Box>
+            </form>
         </div>
     )
 }
--- FixedIncome/frontend/src/components/CreateIssuer.jsx
+++ FixedIncome/frontend/src/components/CreateIssuer.jsx
@@ -6,9 +6,26 @@ import Button from '@mui/material/Button';
 import TextForm from './Forms/TextForm';
 import SelectForm from './Forms/SelectForm';
 import { getData } from 'country-list';
+import { useFormik } from 'formik';
 
 const CreateIssuer = () => {
     const [meta, setMeta] = useState(null)
+    const formik = useFormik({
+        initialValues: {
+            name:'',
+            country:'',
+            industry:'',
+            credit_rating:''
+        },
+
+        onSubmit: (values)=>{
+            AxiosInstance.post('issuers/', values)
+            .then(()=>{
+                console.log('Successfull data submission')
+            })
+        }
+    })
+    console.log(formik.values)
 
     useEffect(()=>{
         AxiosInstance.get('api/meta/').then((res)=>{
@@ -17,27 +34,44 @@ const CreateIssuer = () => {
     },[])
     if (!meta) return null;
 
+
     return (
         <div>
-            <Box className={"TopBar"}>
-                <AddBoxIcon/>
-                <Typography sx={{marginLeft:'15px'}}>Create Issuer</Typography>
-            </Box>
-            <Box sx={{
-                display: 'grid',
-                gridTemplateColumns: { xs: '1fr', md: '1fr 1fr' },
-                gap: '30px',
-                padding: '15px',
-                boxShadow: 'rgba(100,100,111,0.2) 0px 7px 29px 0px',
-            }}>
-                <TextForm label={'Name'} />                                          
-                <SelectForm label={'Country'} options={getData()} valueKey='code' /> 
-                <SelectForm label={'Industry'} options={meta.industries} />          
-                <SelectForm label={'Credit Rating'} options={meta.credit_ratings} /> 
-                <Box sx={{ gridColumn: { md: '1 / -1' } }}>
-                    <Button variant="contained" fullWidth>Submit</Button>
+            <form onSubmit={formik.handleSubmit}>
+                <Box className={"TopBar"}>
+                    <AddBoxIcon/>
+                    <Typography sx={{marginLeft:'15px'}}>Create Issuer</Typography>
                 </Box>
-            </Box>
+                <Box sx={{
+                    display: 'grid',
+                    gridTemplateColumns: { xs: '1fr', md: '1fr 1fr' },
+                    gap: '30px',
+                    padding: '15px',
+                    boxShadow: 'rgba(100,100,111,0.2) 0px 7px 29px 0px',
+                }}>
+                    <TextForm 
+                        label={'Name'} 
+                        name='name' value={formik.values.name}
+                        onChange={formik.handleChange} onBlur={formik.handleBlur}
+                    />                                          
+                    <SelectForm 
+                        label={'Country'} options={getData()} valueKey='code' 
+                        name='country' value={formik.values.country}
+                        onChange={formik.handleChange} onBlur={formik.handleBlur}
+                    /> 
+                    <SelectForm label={'Industry'} options={meta.industries} 
+                        name='industry' value={formik.values.industry}
+                        onChange={formik.handleChange} onBlur={formik.handleBlur}
+                    />          
+                    <SelectForm label={'Credit Rating'} options={meta.credit_ratings} 
+                        name='credit_rating' value={formik.values.credit_rating}
+                        onChange={formik.handleChange} onBlur={formik.handleBlur}
+                    /> 
+                    <Box sx={{ gridColumn: { md: '1 / -1' } }}>
+                        <Button type="submit" variant="contained" fullWidth>Submit</Button>
+                    </Box>
+                </Box>
+            </form>
        </div>
     )
 }
