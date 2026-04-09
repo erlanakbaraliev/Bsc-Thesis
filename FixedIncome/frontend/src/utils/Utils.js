d02c423db9eb20b16611cad9540ed89cc1051de8
Akbaraliev Erlan <erlan.akbaralievich@gmail.com>
2026-04-09 19:15:43 +0200

    Optimized TableView.jsx by dividing into util files

---
+++ FixedIncome/frontend/src/utils/Utils.js
@@ -0,0 +1,29 @@
+// ------------- Ordering ---------------
+const fieldMap = {
+  issuer_country: 'issuer__country',
+  issuer_name: 'issuer__name',
+  credit_rating: 'issuer__credit_rating',
+}
+
+export const getOrdering = (sorting) => {
+  const ordering = sorting.length
+                    ? sorting.map(s => (s.desc ? '-' : '') + (fieldMap[s.id] || s.id)).join(',')
+                    : undefined;
+  return ordering
+}
+
+// ------------- Filtering ---------------
+const textFields = ['isin', 'issuer__country', 'issuer__name', 'bond_type'];
+
+export const getColumnFilters = (columnFilters) => {
+  const filters = columnFilters.reduce((acc, col) => {
+                    const columnName = fieldMap[col.id] || col.id;
+                    if (textFields.includes(columnName)) {
+                      acc[`${columnName}__icontains`] = col.value;
+                    } else {
+                      acc[`${columnName}`] = col.value;
+                    }
+                    return acc;
+                  }, {});
+  return filters
+}
