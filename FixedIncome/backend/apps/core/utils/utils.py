0d3929e42d8602f25a7aadf8073f1d30879eb314
Akbaraliev Erlan <erlan.akbaralievich@gmail.com>
2026-04-24 02:27:22 +0200

    #54: Adds env variable based databases (sqlite, postgresql)

--- FixedIncome/backend/apps/core/utils/utils.py
+++ FixedIncome/backend/apps/core/utils/utils.py
@@ -1,5 +1,6 @@
 import csv
 import io
+import os
 from datetime import datetime
 
 
@@ -48,3 +49,30 @@ def decode_csv_file(file_obj):
     reader.fieldnames = [f.strip() for f in reader.fieldnames]
 
     return reader
+
+
+class SettingsError(Exception):
+    pass
+
+
+DB_ENV = ("sqlite", "postgresql")
+
+
+def get_db_env():
+    db_env = os.getenv("DB_ENV", "postgresql")
+    if db_env not in DB_ENV:
+        raise SettingsError(
+            f"DB_ENV: {db_env} missing from config map. Known envs: {DB_ENV}"
+        )
+    print(f"Database environment: {db_env}")
+    return db_env
+
+
+def create_db_setup(config_map, db_env):
+    try:
+        db_setup = config_map[db_env]
+    except KeyError as exc:
+        raise SettingsError(
+            f"DB_ENV: {db_env} missing from config map. Know evns: {config_map.keys}"
+        ) from exc
+    return db_setup
